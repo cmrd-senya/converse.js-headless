8d8490e75b1cf5c4c3efe5ef8825220376548451
cmrd Senya <senya@riseup.net>
2018-02-23 16:50:57 +0200

    fixup! hackish converse

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 import Converse from 'converse.js/src/headless.js'
 
 export default Converse;
+
+var ConverseInternal = {
+};
+
+export {
+  Converse,
+  ConverseInternal
+}
+
+Converse.plugins.add('expose-internal', {
+  initialize: function () {
+    ConverseInternal._converse = this._converse;
+
+    var handler = ConverseInternal.pluginsInitialized;
+    if (handler) {
+      this._converse.on('pluginsInitialized', handler);
+    }
+  }
+}
